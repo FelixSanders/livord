ef6d623b017d4e8865f264c0cd3c0445a2b933e2
Felix Sanders Kusuma <158176754+FelixSanders@users.noreply.github.com>
2026-06-09 22:14:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,11 +16,7 @@ const currentYear = currentDate.getFullYear();
 
 
 
-if (!localStorage.getItem("emailLogged")) {
-    if (window.location.pathname !== '/livord/login') {
-        window.location.href = '/livord/login';
-    }
-}
+
 
 
 
